7c656a6b01270210a71aa6c22a2511e0f8e90e89
solomononyes <sollysolex@gmail.com>
2021-08-20 08:32:47 +0100

    Commit with md5 hash

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ require('dotenv').config()
 const express = require('express')
 const ejs = require('ejs')
 const mongoose = require('mongoose')
-const encrypt = require('mongoose-encryption')
+// const encrypt = require('mongoose-encryption')
+const md5 = require('md5')
+
 
 const app = express()
 app.use(express.urlencoded({extended: true}))
@@ -26,7 +28,7 @@ const userSchema = new mongoose.Schema({
     required: [true, 'Please enter a password']
   }
 })
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = mongoose.model('User', userSchema)
 
@@ -48,7 +50,7 @@ app.get('/submit', (req, res)=>{
 app.post('/register', (req, res)=>{
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   })
   newUser.save((err)=>{
     if(!err){
@@ -60,7 +62,7 @@ app.post('/register', (req, res)=>{
 })
 app.post('/login', (req, res)=>{
   const userName = req.body.username
-  const password = req.body.password
+  const password = md5(req.body.password)
   User.findOne({email: userName}, (err, foundUser)=>{
     if(foundUser){
       if(foundUser.password === password){
